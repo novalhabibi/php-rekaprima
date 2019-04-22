61596dc88c52a068fc3f8213dfb9d99dbc59a2f4
novalhabibi <34689831+novalhabibi@users.noreply.github.com>
2019-04-22 14:33:56 +0700

    Add nama admin

--- application/views/admin/_includes/navbar.php
+++ application/views/admin/_includes/navbar.php
@@ -11,7 +11,9 @@
                             <a class="nav-link nav-user-img" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="<?= base_url() ?>/assets/admin/assets/images/avatar-1.jpg" alt="" class="user-avatar-md rounded-circle"></a>
                             <div class="dropdown-menu dropdown-menu-right nav-user-dropdown" aria-labelledby="navbarDropdownMenuLink2">
                                 <div class="nav-user-info">
-                                    <h5 class="mb-0 text-white nav-user-name">John Abraham </h5>
+                                    <h5 class="mb-0 text-white nav-user-name">
+                                        <?= $this->session->userdata('nama_admin') ?>
+                                    </h5>
                                     <span class="status"></span><span class="ml-2">Available</span>
                                 </div>
                                 <a class="dropdown-item" href="#"><i class="fas fa-user mr-2"></i>Account</a>
